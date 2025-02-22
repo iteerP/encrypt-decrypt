3c3093b473062dcac9cc57b23e85ff8018713865
pgobin <pgobin@uw.edu>
2025-02-22 00:39:14 -0800

    small edit to card component

--- app/components/(SelectSubstitutionAlgorithmsPage)/Card.tsx
+++ app/components/(SelectSubstitutionAlgorithmsPage)/Card.tsx
@@ -17,7 +17,7 @@ export default function SubstitutionAlgorithmCard({
       initial={{ y: "-200vh" }}
       animate={{ y: 0 }}
       transition={{ delay: delay, ease: "easeOut", duration: 1 }}
-      className="w-full aspect-[4/3] h-full min-h-[200px] sm:min-h-[200px] bg-blue-300 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg"
+      className="w-full aspect-[4/3] h-full min-h-[200px] sm:min-h-[200px] bg-blue-300 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg hover:bg-blue-400 duration-300 cursor-pointer"
     >
       <p className="select-none">{algorithmName}</p>
     </motion.div>
