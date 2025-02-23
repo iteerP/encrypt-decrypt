dd4ee3e0983519528634c78e2a6e14f34ca677f7
pgobin <pgobin@uw.edu>
2025-02-22 16:24:34 -0800

    refactor atbash cipher function

--- app/utility/substitution-algorithms/atbash-cipher.tsx
+++ app/utility/substitution-algorithms/atbash-cipher.tsx
@@ -1,8 +1,41 @@
 function atbashCipherEncrypt(text: string) {
-  return text.replace(/[a-zA-Z]/g, (char) => {
-    const base = char < "a" ? 65 : 97;
-    return String.fromCharCode(base + (25 - (char.charCodeAt(0) - base)));
-  });
+  text = text.toLowerCase();
+
+  const mappings: Record<string, string> = {
+    a: "z",
+    b: "y",
+    c: "x",
+    d: "w",
+    e: "v",
+    f: "u",
+    g: "t",
+    h: "s",
+    i: "r",
+    j: "q",
+    k: "p",
+    l: "o",
+    m: "n",
+    n: "m",
+    o: "l",
+    p: "k",
+    q: "j",
+    r: "i",
+    s: "h",
+    t: "g",
+    u: "f",
+    v: "e",
+    w: "d",
+    x: "c",
+    y: "b",
+    z: "a",
+  };
+
+  const encryptedText = [];
+  for (let idx = 0; idx < text.length; idx++) {
+    encryptedText.push(mappings[text[idx]]);
+  }
+
+  return encryptedText.join("");
 }
 
 export default atbashCipherEncrypt;
